8b64023b225b96a5a9ee36fc7d2506d3986a8985
Son Hai Nguyen <sirarseus@gmail.com>
2014-09-25 21:20:19 +0200

    New: registred new type - RectangleContainer

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <QQmlApplicationEngine>
 
 #include "types/game.h"
+#include "types/containers/rectanglecontainer.h"
 #include "types/singleObjects/gamerectangle.h"
 
 int main(int argc, char *argv[])
@@ -10,6 +11,7 @@ int main(int argc, char *argv[])
 
     qmlRegisterType<Game>("Game", 1, 0, "Game");
     qmlRegisterType<GameRectangle>("GameRectangle", 1, 0, "GameRectangle");
+    qmlRegisterType<RectangleContainer>("RectangleContainer", 1, 0, "RectangleContainer");
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));

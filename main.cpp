b3383cf1a5f0fbba1a98c9a66dcd2308227280e2
Son Hai Nguyen <sirarseus@gmail.com>
2014-09-25 08:12:33 +0200

    New: registered new objects

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <QApplication>
 #include <QQmlApplicationEngine>
 
+#include "types/game.h"
+#include "types/singleObjects/gamerectangle.h"
+
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+
+    qmlRegisterType<Game>("Game", 1, 0, "Game");
+    qmlRegisterType<GameRectangle>("GameRectangle", 1, 0, "GameRectangle");
 
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
--- main.qml
+++ main.qml
@@ -1,8 +1,19 @@
 import QtQuick 2.3
 import QtQuick.Controls 1.2
 
+import Game 1.0
+import RectangleContainer 1.0
+
 ApplicationWindow {
     visible: true
     width: 854
     height: 480
+
+    Game {
+        id: game
+
+        RectangleContainer {
+            id: container
+        }
+    }
 }
